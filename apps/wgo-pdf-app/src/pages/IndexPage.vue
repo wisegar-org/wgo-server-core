27e2b5425b3b2e980ae725ba4619bbcd2cc5bf12
Yariel Rodriguez <yariel.re@gmail.com>
2025-09-04 12:48:00 +0200

    Upload pdf apps

---
+++ apps/wgo-pdf-app/src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-uploader
+      url="http://localhost:4444/upload"
+      label="Restricted to images"
+      accept=".jpg, image/*"
+      style="max-width: 500px"
+    ></q-uploader>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
